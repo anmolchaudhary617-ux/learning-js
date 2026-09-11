c63e23493d5c81fed3daef6e9aca13e0ef82756f
Anmol Chaudhary <anmolchaudhary617@gmail.com>
2026-09-11 18:08:38 +0530

    Practice the 3 languages together Part 5

---
+++ lesson10/10e/10e.js
@@ -0,0 +1,51 @@
+const gaming = document.querySelector('.gaming-button');
+const music = document.querySelector('.music-button');
+const tech = document.querySelector('.tech-button');
+
+function changeApperance1() {
+  if (gaming.classList.contains('is-on')) {
+  gaming.classList.remove('is-on');
+  gaming.classList.add('is-off');
+}
+else {
+  gaming.classList.remove('is-off');
+  gaming.classList.add('is-on');
+}
+
+  if (gaming.innerText === 'Gaming') {
+    gaming.innerText = 'Start Gaming';
+  }
+  else gaming.innerText = 'Gaming';
+}
+
+function changeApperance2() {
+  if (music.classList.contains('is-on')) {
+    music.classList.remove('is-on');
+    music.classList.add('is-off');
+  }
+  else {
+    music.classList.remove('is-off');
+    music.classList.add('is-on');
+  }
+
+  if (music.innerText === 'Music') {
+    music.innerText = 'Start Music';
+  }
+  else music.innerText = 'Music';
+}
+
+function changeApperance3() {
+  if (tech.classList.contains('is-on')) {
+    tech.classList.remove('is-on');
+    tech.classList.add('is-off');
+  }
+  else {
+    tech.classList.remove('is-off');
+    tech.classList.add('is-on');
+  }
+
+  if (tech.innerText === 'Tech') {
+    tech.innerText = 'Do Tech';
+  }
+  else tech.innerText = 'Tech';
+}
